84de68c637d089222b5522811b87c6a909ef6edc
luixmartinv2 <luixmartinv2@gmail.com>
2016-11-15 12:37:26 +0100

    algo

---
+++ Actividad1 copia/app/src/main/java/com/example/luistallafigo/actividad1/Actividad1.java
@@ -0,0 +1,21 @@
+package com.example.luistallafigo.actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Actividad1 extends AppCompatActivity implements View.OnClickListener{
+
+    Actividad1 vista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad1);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()) == vista.btn
+    }
+}
---
+++ Actividad1 copia/app/src/main/java/com/example/luistallafigo/actividad1/Actividad1C.java
@@ -0,0 +1,11 @@
+package com.example.luistallafigo.actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by luis.tallafigo on 08/11/2016.
+ */
+public class Actividad1C extends AppCompatActivity {
+    this.vista=vista;
+
+}
